f8065aa237a484292298c9061d06051057fdf0bf
wttjoao <joaoalves1102@outlook.pt>
2025-07-24 18:33:47 +0100

    First Commit

---
+++ src/main/java/com/joaoalves/userregistrationservice/UserRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joaoalves.userregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaoalves/userregistrationservice/UserRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoalves.userregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
